80f3ff83aa6e82c766be7f8a1ef4708237479fb0
Indigo369Christian <kersten786@gmail.com>
2025-09-17 10:15:09 +0200

    Add files via upload

---
+++ TelosIndigo_Deploy_Full_v2.0/sw.js
@@ -0,0 +1,46 @@
+/* TelosIndigo minimal service worker */
+const CACHE_NAME = 'telosindigo-v2';
+const ASSETS = [
+  './',
+  'index.html',
+  'manifest.json',
+  'apple-touch-icon.png',
+  'icon-192.png',
+  'icon-512.png',
+  'TelosIndigo_OG_1200x630.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : undefined)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.headers.get('accept')?.includes('text/html')) {
+    event.respondWith(
+      fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+        return res;
+      }).catch(() => caches.match(req))
+    );
+  } else {
+    event.respondWith(
+      caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+        return res;
+      }))
+    );
+  }
+});
